2a51871deeb0787f6baaf189567ab11d0a75c96b
Philip Lipman <philip.lipman@gmail.com>
2015-06-01 19:51:49 -0400

    Updating UrizaFrontend files

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-drag-and-drop-lists/angular-drag-and-drop-lists.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
